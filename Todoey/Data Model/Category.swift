21a4ffe4cdf815c37fccc3a20cc6e60ef54de357
Marina Svistkova <svmrin1@gmail.com>
2019-11-26 17:33:23 +0300

    beginning of using Realm (CoreData can still be restored)

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Marina Svistkova on 26.11.2019.
+//  Copyright © 2019 Marina Svistkova. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Marina Svistkova on 26.11.2019.
+//  Copyright © 2019 Marina Svistkova. All rights reserved.
+//
+
+import Foundation
